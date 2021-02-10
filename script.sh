b63d00afaee9ab362a98883efb629fc9a2fea7a8
Emre Savaş <e@emresavas.com>
2021-02-11 01:53:09 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,3 +2,5 @@
 export DOMAIN="$(uapi DomainInfo domains_data | grep -w "domain")"
 export DOMAIN="${DOMAIN#*: }"
 echo $DOMAIN
+uapi PassengerApps register_application name="nodejsapp" path="/nodejsapp" domain="$DOMAIN" deployment_mode="production" base_uri="/nodejsapp" enabled="1"
+uapi PassengerApps ensure_deps type="npm" app_path="nodejsapp"
